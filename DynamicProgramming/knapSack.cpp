ccf777a546b6b1ad9bde0e686d65d06f11cf82c0
Ayushchauhan10 <97461020+Ayushchauhan10@users.noreply.github.com>
2022-12-28 11:49:42 +0530

    Create knapSack.cpp

---
+++ DynamicProgramming/knapSack.cpp
@@ -0,0 +1,76 @@
+/*
+Code: Knapsack (Memoization and DP)
+
+A thief is robbing a store and can carry a maximal weight of W into his knapsack. There are N items and ith item weighs wi and is of value vi. Considering the constraints of maximum weight that knapsack can carry, you have to find and return the maximum value that thief can generate by stealing items.
+Note
+Space complexity should be O(W).
+Input Format :
+The first line contains an integers, that denote the value of N. The following line contains N space separated integers, that denote the values of weight of items. The following line contains N space separated integers, that denote the values associated with the items. The following line contains an integer that denote the value of W. W denotes maximum weight that thief can carry.
+Output Format :
+The first and only line of output contains the maximum value that thief can generate, as described in the task. 
+Constraints
+1 <= N <= 10^4
+1<= wi <= 100
+1 <= vi <= 100
+Time Limit: 1 second
+Sample Input 1 :
+4
+1 2 4 5
+5 4 8 6
+5
+Sample Output 1 :
+13
+*/
+
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int knapsack(int *wt, int *val, int n, int W)
+{ 
+     int i, w;
+     vector<vector<int>> K(n + 1, vector<int>(W + 1));
+
+    for(i = 0; i <= n; i++)
+    {
+        for(w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] +
+                                K[i - 1][w - wt[i - 1]],
+                                K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    
+ return K[n][W];   
+}
+
+
+int main() 
+{
+    int n;
+    cin >> n;
+    int* wt = new int[n];
+    int* val = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> wt[i];
+    }
+
+    for (int j = 0; j < n; j++) {
+        cin >> val[j];
+    }
+
+    int w;
+    cin >> w;
+
+    cout << knapsack(wt, val, n, w) << "\n";
+
+    delete[] wt;
+    delete[] val;
+}
+
